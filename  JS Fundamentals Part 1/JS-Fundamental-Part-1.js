87ed0de99502ab1ec4bd4b2a4c7729d56165d820
Hanieh Ghassemi <89027450+HaniehGhassemi@users.noreply.github.com>
2023-02-05 13:18:36 +0330

    js code challengs for js fundamental p1

---
+++  JS Fundamentals Part 1/JS-Fundamental-Part-1.js
@@ -0,0 +1,48 @@
+////////////////////////////////////////////Code Challenge #1////////////////////////////////////////////
+
+// const weightMark = 78;
+// const weightJohn = 92;
+// const heighMark = 1.69;
+// const heightJohn = 1.95;
+
+// const BMIMark = weightMark / heighMark ** 2;
+// const BMIJohn = weightJohn / (heightJohn * heightJohn);
+// const markHigherBMI = BMIMark > BMIJohn;
+// console.log(markHigherBMI);
+
+////////////////////////////////////////////Code Challenge #2////////////////////////////////////////////
+
+// const weightMark = 85;
+// const weightJohn = 95;
+// const heighMark = 1.69;
+// const heightJohn = 1.76;
+
+// const BMIMark = weightMark / heighMark ** 2;
+// const BMIJohn = weightJohn / (heightJohn * heightJohn);
+// console.log("BMI Mark's is :" + BMIMark, "BMI John's is :" + BMIJohn);
+
+// if (BMIMark > BMIJohn) {
+//   console.log(`Mark's BMI is (${BMIMark}) heighter than John's (${BMIJohn})`);
+// } else {
+//   console.log(`John's BMI is (${BMIJohn}) heighter than Mark's (${BMIMark})`);
+// }
+
+////////////////////////////////////////////Code Challenge #3////////////////////////////////////////////
+
+
+// const dolphinsScore = (96 + 108 + 89) / 3;
+// const koalasScore = (88 + 91 + 110) / 3;
+
+// console.log("Dolphins Score is: " + dolphinsScore, "Koalas Score is: " + koalasScore);
+
+// if (dolphinsScore > koalasScore && dolphinsScore >= 100) {
+//     console.log("Dolphins team is winner");
+// } else if (koalasScore > dolphinsScore && koalasScore >= 100) {
+//     console.log("Koalas team is winner");
+// } else if (dolphinsScore >= 100 && dolphinsScore === koalasScore) {
+//     console.log("both of teams are winner");
+// } else {
+//     console.log("none of the teams won");
+// }
+
+////////////////////////////////////////////Code Challenge #4////////////////////////////////////////////
